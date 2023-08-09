12a5b769030bf8d3b936c604948f08eff26d9806
Mike Rodelas <mikerodelas@digits.ph>
2023-08-09 16:36:05 +0800

    update web route

--- routes/web.php
+++ routes/web.php
@@ -37,16 +37,16 @@ Route::get('digits_sales/template',[DigitsSaleController::class, 'uploadTemplate
 Route::post('digits_sales/export',[DigitsSaleController::class, 'exportSales'])->name('digits-sales.export');
 
 //import store inventory
-Route::post('store_inventory/import-upload',[StoreInventoryController::class, 'storeInventoryUpload'])->name('store-inventory.upload');
-Route::get('store_inventory/import',[StoreInventoryController::class, 'storeInventoryUploadView'])->name('store-inventory.upload-view');
-Route::get('store_inventory/template',[StoreInventoryController::class, 'uploadTemplate'])->name('store-inventory.template');
-Route::post('store_inventory/export',[StoreInventoryController::class, 'exportInventory'])->name('store-inventory.export');
+Route::post('store_inventories/import-upload',[StoreInventoryController::class, 'storeInventoryUpload'])->name('store-inventory.upload');
+Route::get('store_inventories/import',[StoreInventoryController::class, 'storeInventoryUploadView'])->name('store-inventory.upload-view');
+Route::get('store_inventories/template',[StoreInventoryController::class, 'uploadTemplate'])->name('store-inventory.template');
+Route::post('store_inventories/export',[StoreInventoryController::class, 'exportInventory'])->name('store-inventory.export');
 
 //import warehouse inventory
-Route::post('warehouse_inventory/import-upload',[WarehouseInventoryController::class, 'warehouseInventoryUpload'])->name('warehouse-inventory.upload');
-Route::get('warehouse_inventory/import',[WarehouseInventoryController::class, 'warehouseInventoryUploadView'])->name('warehouse-inventory.upload-view');
-Route::get('warehouse_inventory/template',[WarehouseInventoryController::class, 'uploadTemplate'])->name('warehouse-inventory.template');
-Route::post('warehouse_inventory/export',[WarehouseInventoryController::class, 'exportInventory'])->name('warehouse-inventory.export');
+Route::post('warehouse_inventories/import-upload',[WarehouseInventoryController::class, 'warehouseInventoryUpload'])->name('warehouse-inventory.upload');
+Route::get('warehouse_inventories/import',[WarehouseInventoryController::class, 'warehouseInventoryUploadView'])->name('warehouse-inventory.upload-view');
+Route::get('warehouse_inventories/template',[WarehouseInventoryController::class, 'uploadTemplate'])->name('warehouse-inventory.template');
+Route::post('warehouse_inventories/export',[WarehouseInventoryController::class, 'exportInventory'])->name('warehouse-inventory.export');
 
 Route::post('report_privileges/get/table-columns',[AdminReportPrivilegesController::class, 'getTableColumns'])->name('report-privileges.getTableColumns');
 Route::post('report_privileges/create/save',[AdminReportPrivilegesController::class, 'saveReport'])->name('report-privileges.save');
