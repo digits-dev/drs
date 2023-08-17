5963e51a5c5b5529d1413e8a0be2d55188963cf2
Mike Rodelas <mikerodelas@digits.ph>
2023-08-17 15:27:48 +0800

    adjust cron timeout to 600

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('queue:work --tries=3 --timeout=60')->everyMinute();
+        $schedule->command('queue:work --tries=3 --timeout=600')->everyMinute();
     }
 
     /**
