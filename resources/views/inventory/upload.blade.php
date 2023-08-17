1c41265e4f39356aaf4f46a8ff3f12321a1633e9
Mike Rodelas <mikerodelas@digits.ph>
2023-08-17 11:22:26 +0800

    add error label

--- resources/views/inventory/upload.blade.php
+++ resources/views/inventory/upload.blade.php
@@ -32,6 +32,7 @@
         @if($errors->any())
         <div class="alert alert-danger" role="alert">
             <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+            <h4>Errors!</h4>
             <ul>
                 @foreach ($errors->all() as $error)
                     <li>{{ $error }}</li>
