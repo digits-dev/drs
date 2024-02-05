6b2c207460e730feb2b6f99dcd8c0e295004eefd
Fillinor <fillinorgunio@digits.ph>
2024-02-05 15:19:46 +0800

    create migration for admin_items and service_items

---
+++ database/migrations/2024_02_05_144827_create_service_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateServiceItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('service_items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('item_code')->nullable();
+            $table->string('item_description')->nullable();
+            $table->decimal('current_srp', 16, 2)->nullable();
+            $table->string('status')->length(20)->default('ACTIVE')->nullable();
+            $table->mediumInteger('created_by')->unsigned()->nullable();
+            $table->mediumInteger('updated_by')->unsigned()->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('service_items');
+    }
+}
