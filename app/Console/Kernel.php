c4ea168f2e2ca39a7bbbdf77345dc4e785a590d9
Mike Rodelas <mikerodelas@digits.ph>
2024-05-07 09:02:30 +0800

    adjust digits sales index view query

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,8 +19,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        // $schedule->command('queue:work --tries=3 --timeout=600')->everyMinute();
-        $schedule->command('queue:check')->everyMinute();
+        $schedule->command('queue:check')->everyMinute()->withoutOverlapping();
     }
 
     /**
