8cd804028490e3b844c04461fac4682e8a3552e8
DTCRepo <marvinmosico@digits.ph>
2024-06-18 17:01:10 +0800

    add migration

--- database/migrations/2024_06_18_125552_add_columns_to_admin_items.php
+++ database/migrations/2024_06_18_125552_add_columns_to_admin_items.php
@@ -29,23 +29,7 @@ class AddColumnsToAdminItems extends Migration
         //     $table->date('initial_wrr_date')->nullable()->after('current_srp');
         // });
 
-        Schema::table('rma_items', function (Blueprint $table) {
-            $table->string('upc_code')->nullable()->after('digits_code');
-            $table->string('upc_code2')->nullable()->after('upc_code');
-            $table->string('upc_code3')->nullable()->after('upc_code2');
-            $table->string('upc_code4')->nullable()->after('upc_code3');
-            $table->string('upc_code5')->nullable()->after('upc_code4');
-            $table->string('brand_description')->nullable()->after('upc_code5');
-            $table->string('category_description')->nullable()->after('brand_description');
-            $table->string('margin_category_description')->nullable()->after('category_description');
-            $table->string('vendor_type_code')->nullable()->after('margin_category_description');
-            $table->string('inventory_type_description')->nullable()->after('vendor_type_code');
-            $table->string('sku_status_description')->nullable()->after('inventory_type_description');
-            $table->string('brand_status')->nullable()->after('sku_status_description');
-            $table->date('initial_wrr_date')->nullable()->after('current_srp');
-        });
-
-        // Schema::table('gacha_items', function (Blueprint $table) {
+        // Schema::table('rma_items', function (Blueprint $table) {
         //     $table->string('upc_code')->nullable()->after('digits_code');
         //     $table->string('upc_code2')->nullable()->after('upc_code');
         //     $table->string('upc_code3')->nullable()->after('upc_code2');
@@ -60,6 +44,22 @@ class AddColumnsToAdminItems extends Migration
         //     $table->string('brand_status')->nullable()->after('sku_status_description');
         //     $table->date('initial_wrr_date')->nullable()->after('current_srp');
         // });
+
+        Schema::table('gacha_items', function (Blueprint $table) {
+            $table->string('upc_code')->nullable()->after('digits_code');
+            $table->string('upc_code2')->nullable()->after('upc_code');
+            $table->string('upc_code3')->nullable()->after('upc_code2');
+            $table->string('upc_code4')->nullable()->after('upc_code3');
+            $table->string('upc_code5')->nullable()->after('upc_code4');
+            $table->string('brand_description')->nullable()->after('upc_code5');
+            $table->string('category_description')->nullable()->after('brand_description');
+            $table->string('margin_category_description')->nullable()->after('category_description');
+            $table->string('vendor_type_code')->nullable()->after('margin_category_description');
+            $table->string('inventory_type_description')->nullable()->after('vendor_type_code');
+            $table->string('sku_status_description')->nullable()->after('inventory_type_description');
+            $table->string('brand_status')->nullable()->after('sku_status_description');
+            $table->date('initial_wrr_date')->nullable()->after('current_srp');
+        });
 
         Schema::table('service_items', function (Blueprint $table) {
             $table->string('upc_code')->nullable()->after('item_code');
@@ -101,23 +101,7 @@ class AddColumnsToAdminItems extends Migration
         //     $table->dropColumn('initial_wrr_date');
         // });
 
-        Schema::table('rma_items', function (Blueprint $table) {
-            $table->dropColumn('upc_code');
-            $table->dropColumn('upc_code2');
-            $table->dropColumn('upc_code3');
-            $table->dropColumn('upc_code4');
-            $table->dropColumn('upc_code5');
-            $table->dropColumn('brand_description');
-            $table->dropColumn('category_description');
-            $table->dropColumn('margin_category_description');
-            $table->dropColumn('vendor_type_code');
-            $table->dropColumn('inventory_type_description');
-            $table->dropColumn('sku_status_description');
-            $table->dropColumn('brand_status');
-            $table->dropColumn('initial_wrr_date');
-        });
-
-        // Schema::table('gacha_items', function (Blueprint $table) {
+        // Schema::table('rma_items', function (Blueprint $table) {
         //     $table->dropColumn('upc_code');
         //     $table->dropColumn('upc_code2');
         //     $table->dropColumn('upc_code3');
@@ -132,6 +116,22 @@ class AddColumnsToAdminItems extends Migration
         //     $table->dropColumn('brand_status');
         //     $table->dropColumn('initial_wrr_date');
         // });
+
+        Schema::table('gacha_items', function (Blueprint $table) {
+            $table->dropColumn('upc_code');
+            $table->dropColumn('upc_code2');
+            $table->dropColumn('upc_code3');
+            $table->dropColumn('upc_code4');
+            $table->dropColumn('upc_code5');
+            $table->dropColumn('brand_description');
+            $table->dropColumn('category_description');
+            $table->dropColumn('margin_category_description');
+            $table->dropColumn('vendor_type_code');
+            $table->dropColumn('inventory_type_description');
+            $table->dropColumn('sku_status_description');
+            $table->dropColumn('brand_status');
+            $table->dropColumn('initial_wrr_date');
+        });
 
         Schema::table('service_items', function (Blueprint $table) {
             $table->dropColumn('upc_code');
