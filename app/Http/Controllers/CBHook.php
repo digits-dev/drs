60e874d423db8ef16019512b917bbeb937b9b25c
MarvinMosiCoder <marvsmosico@gmail.com>
2024-09-25 14:15:10 +0800

    change cb hook

--- app/Http/Controllers/CBHook.php
+++ app/Http/Controllers/CBHook.php
@@ -16,18 +16,18 @@ class CBHook extends Controller {
 	|
 	*/
 	public function afterLogin() {
-		$user =  $users = DB::table('cms_users')->where("id", CRUDbooster::myId())->first();
+		$user =  DB::table('cms_users')->where("id", CRUDbooster::myId())->first();
 		if ($user->last_password_updated) {
 			// Compare the password updated date with the current date
 			$passwordLastUpdated = Carbon::parse($user->last_password_updated);
 	
 			if ($passwordLastUpdated->diffInMonths(Carbon::now()) > 3) {
 				// Password is older than 3 months
-				Session::put('password_is_old', $users->last_password_updated);
+				Session::put('password_is_old', $user->last_password_updated);
 			}else{
 				Session::put('password_is_old', '');
 			}
 		}
-		Session::put('admin_password', $users->password);
+		Session::put('admin_password', $user->password);
 	}
 }
