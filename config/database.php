9776724ff175325b9942f5dcf93774132fb8dbcf
MarvinMosiCoder <marvsmosico@gmail.com>
2024-10-11 10:53:51 +0800

    Done store sales etp data

--- config/database.php
+++ config/database.php
@@ -102,6 +102,26 @@ return [
             ],
         ],
 
+        'aimfs' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_AIMFS_URL'),
+            'host' => env('DB_AIMFS_HOST', '127.0.0.1'),
+            'port' => env('DB_AIMFS_PORT', '3306'),
+            'database' => env('DB_AIMFS_DATABASE', 'forge'),
+            'username' => env('DB_AIMFS_USERNAME', 'forge'),
+            'password' => env('DB_AIMFS_PASSWORD', ''),
+            'unix_socket' => env('DB_AIMFS_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) :[],
+        ],
+
         'masterfile' => [
             'driver' => 'mysql',
             'host' => env('DB_MASTERFILE_HOST', '127.0.0.1'),
